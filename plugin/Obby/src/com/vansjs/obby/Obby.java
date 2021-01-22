477aedffa6af331a6dfcbe637c15dc67cf35b88b
TheYushinator <ayushtvora@gmail.com>
2021-01-21 19:15:25 -0500

    Error in previous push

--- plugin/Obby/src/com/vansjs/obby/Obby.java
+++ plugin/Obby/src/com/vansjs/obby/Obby.java
@@ -1,2 +1,20 @@
-package com.vansjs.obby;public class Obby {
+package com.vansjs.obby;
+
+import com.vansjs.obby.events.Events;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Obby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Events(), this);
+
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Plugin Started");
+    }
+
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "Plugin Stopped.");
+    }
 }
--- plugin/Obby/src/com/vansjs/obby/events/Events.java
+++ plugin/Obby/src/com/vansjs/obby/events/Events.java
@@ -1,2 +1,62 @@
-package com.vansjs.obby.events;public class Events {
+package com.vansjs.obby.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class Events implements Listener {
+
+//    @EventHandler
+//    public static void onPlayerJoin(PlayerJoinEvent event) {
+//        Player player = event.getPlayer();
+//        player.sendMessage(ChatColor.LIGHT_PURPLE + "Welcome to the Server!! :)");
+//    }
+
+//    @EventHandler
+//    public static void onPlayerWalk(PlayerMoveEvent event) {
+//        Player player = event.getPlayer();
+//
+//        int x = player.getLocation().getBlockX();
+//        int y = player.getLocation().getBlockY();
+//        int z = player.getLocation().getBlockZ();
+//
+//        Material block = player.getWorld().getBlockAt(x, y-1, z).getType();
+//
+//        if (block == Material.STONE) {
+//            player.sendMessage(ChatColor.GREEN + "You are standing on Stone!");
+//        }
+//    }
+//
+
+    @EventHandler
+    public static void getPlayerDirection(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+
+        float yawF = player.getLocation().getYaw();
+        int yaw = (int) Math.floor(yawF);
+
+        if (yaw < 0) {
+            yaw += 360;
+        }
+
+        player.sendMessage(ChatColor.GOLD + "You are facing: " + yaw);
+
+    }
+
+    @EventHandler
+    public static void getPlayerHealth(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+
+        double healthF = player.getHealth();
+        int health = (int) Math.ceil(healthF);
+
+        player.sendMessage(ChatColor.DARK_RED + "Your health is at " + health);
+    }
+
 }
